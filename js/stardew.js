4e213a6df8f74e2d45c9da7a1abafe6d4afae092
AkiVonAkira <akivonakira@gmail.com>
2022-11-09 15:20:52 +0100

    wip image slider

--- js/stardew.js
+++ js/stardew.js
@@ -40,103 +40,76 @@ async function getPageData() {
         const figureIMG = contentFigure.appendChild(document.createElement('img'));
         figureIMG.src = data.main[0].image;
 
-        for (const list of data.content) {
+        for (const array of data.content) {
+            console.log(array);
             const contentSection = content.appendChild(document.createElement('section'));
             const contentText = contentSection.appendChild(document.createElement('div'));
             contentText.classList = "content-text";
 
             const contentTextH1 = contentText.appendChild(document.createElement('h1'));
-            const contentTextP = contentText.appendChild(document.createElement('p'));
-            contentTextH1.textContent = list.title;
-            contentTextP.innerHTML = `${list.paragraph}`;
+            if (array.paragraph) {
+                const contentTextP = contentText.appendChild(document.createElement('p'));
+                contentTextP.innerHTML = `${array.paragraph}`;
+            }
+            contentTextH1.textContent = array.title;
 
-            const imageGrid = contentSection.appendChild(document.createElement('div'));
-            imageGrid.classList = "image-grid";
-            for (const image of list.image) {
-                console.log(image);
-                const contentFigure = imageGrid.appendChild(document.createElement('figure'));
-                const figureIMG = contentFigure.appendChild(document.createElement('img'));
+            const slideshowContainer = contentSection.appendChild(document.createElement('div'));
+            slideshowContainer.classList = "slideshow-container";
+
+            const prev = slideshowContainer.appendChild(document.createElement('a'));
+            prev.classList = "prev";
+            prev.onclick = `plusSlides(-1)`;
+            prev.textContent = "❮";
+
+
+            for (const image of array.image) {
+                const mySlides = slideshowContainer.appendChild(document.createElement('div'));
+                const figureIMG = mySlides.appendChild(document.createElement('img'));
                 figureIMG.src = image;
+                mySlides.classList = "mySlides fade";
+            }
+
+
+            const next = slideshowContainer.appendChild(document.createElement('a'));
+            next.classList = "next";
+            next.onclick = `plusSlides(1)`;
+            next.textContent = "❯";
+
+
+            const dotContainer = contentSection.appendChild(document.createElement('div'));
+            dotContainer.classList = "dot-container"
+            for (let j = 0; j < array.image.length; j++) {
+                const dot = dotContainer.appendChild(document.createElement('span'));
+                dot.classList = "dot";
+                dot.onclick = `${currentSlide(j)}`;
             }
         }
     }
+}
 
+let slideIndex = 0;
+showSlides(slideIndex);
 
-    {/* <div class="content" id="content-stardew">
-    <section>
-        <div class="content-text">
-            <h1>
-                Stardew Valley Copy
-            </h1>
-            <a href="portfolio.html">./Portfolio</a>
-            <p>
-                I was never able to finish the project but I am happy with the amount of features I was able
-                to implement in the short time frame I worked on it. <br>
-                Below are some GIF's and Images of the project.
-            </p>
-        </div>
-        <div class="stardew-image">
-            <figure>
-                <img src="img/stardew/17.18.15Unity_g1jcWO2hc7.png" alt="image">
-            </figure>
-        </div>
-    </section>
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
-    <section>
-        <div class="content-text">
-            <h1>
-                GIF Showcases
-            </h1>
-            <p>
-                Here you can see the character doing various stuff, such as;
-                Breaking wood, tilling land, planting crop, talking to an NPC, showcasing
-                crafting and going to a different zone.
-            </p>
-        </div>
-    </section>
-    <section>
-        <div class="image-grid">
-            <figure>
-                <img src="img/stardew/08.03.22_P3oNlX32TO.gif" alt="image">
-            </figure>
-            <figure>
-                <img src="img/stardew/18.05.57_DBBbdqj0GP.gif" alt="image">
-            </figure>
-            <figure>
-                <img src="img/stardew/18.03.54_f0pIIqinY8.gif" alt="image">
-            </figure>
-        </div>
-    </section>
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
-    <section>
-        <div class="content-text">
-            <h1>
-                Screenshots
-            </h1>
-        </div>
-    </section>
-    <section>
-        <div class="image-grid">
-            <figure>
-                <img src="img/stardew/17.18.15Unity_g1jcWO2hc7.png" alt="image">
-            </figure>
-            <figure>
-                <img src="img/stardew/17.18.16Unity_Bh2Ks1Ayoc.png" alt="image">
-            </figure>
-            <figure>
-                <img src="img/stardew/17.18.19Unity_s7DluiHdff.png" alt="image">
-            </figure>
-            <figure>
-                <img src="img/stardew/17.18.22Code_9R2bYCOIYn.png" alt="image">
-            </figure>
-            <figure>
-                <img src="img/stardew/18.06.08Unity_6vn4qArHbc.png" alt="image">
-            </figure>
-            <figure>
-                <img src="img/stardew/18.06.23Unity_WGKS560KSv.png" alt="image">
-            </figure>
-        </div>
-    </section>
-</div>
-</div> */}
+function showSlides(n) {
+    let i;
+    let slides = document.querySelectorAll(".mySlides");
+    let dots = document.querySelectorAll(".dot");
+    if (n > slides.length) { slideIndex = 0 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
