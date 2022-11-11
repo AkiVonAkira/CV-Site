e6dafad1057cfa5a5cfc4a69d996182854d666b9
AkiVonAkira <akivonakira@gmail.com>
2022-11-11 06:31:43 +0100

    pages footer fix

--- js/footer.js
+++ js/footer.js
@@ -8,13 +8,18 @@ export async function getfooterData() {
         const footer = body.appendChild(document.createElement('footer'));
         for (const list of data.content) {
             const container = footer.appendChild(document.createElement('ul'));
+            container.classList = "footer-list-container";
             const title = container.appendChild(document.createElement('h1'));
+            title.classList = "footer-title";
             title.textContent = `${list.title}`;
             for (const info of list.info) {
                 const infoContainer = container.appendChild(document.createElement('li'));
+                infoContainer.classList = "footer-list";
                 if (info.img) {
                     const element = infoContainer.appendChild(document.createElement('a'));
+                    element.classList = "footer-list-item";
                     const img = element.appendChild(document.createElement('img'));
+                    img.classList = "footer-img";
                     img.src = info.img
                     element.innerHTML += info.desc
                     if (info.reference) {
